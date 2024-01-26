b544b44d5d194a80465f3581ec16616df2bc7787
Gleidson Queiroz <gldsnqueiroz2@gmail.com>
2024-01-25 21:37:02 -0300

    filter reduce

---
+++ public/app/app.js
@@ -0,0 +1,13 @@
+import { handleStatus, log } from "./utils/promise-help.js";
+
+document.querySelector("#myButton").onclick = () =>
+  fetch("http://localhost:3000/notas")
+    .then(handleStatus)
+    .then((notas) =>
+      notas.reduce((array, nota) => array.concat(nota.itens), []))
+    .then(log)
+    .then((itens) => itens.filter((item) => item.codigo == "2143"))
+    .then(log)
+    .then((itens) => itens.reduce((total, item) => total + item.valor, 0))
+    .then(console.log)
+    .catch(console.log);
