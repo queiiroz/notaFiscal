c268dedb1760a86804c9bbfa7030924f952856f7
Gleidson Queiroz <gldsnqueiroz2@gmail.com>
2024-01-25 21:46:29 -0300

    funcao flatMap

--- public/app/app.js
+++ public/app/app.js
@@ -1,10 +1,10 @@
 import { handleStatus, log } from "./utils/promise-help.js";
+import "./utils/array-help.js";
 
 document.querySelector("#myButton").onclick = () =>
   fetch("http://localhost:3000/notas")
     .then(handleStatus)
-    .then((notas) =>
-      notas.reduce((array, nota) => array.concat(nota.itens), []))
+    .then((notas) => notas.$flatMap((nota) => nota.itens))
     .then(log)
     .then((itens) => itens.filter((item) => item.codigo == "2143"))
     .then(log)
