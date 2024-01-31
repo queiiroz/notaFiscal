7274c5a3a97b07c349429cd2ad22cbcf92fb9ac6
Gleidson Queiroz <gldsnqueiroz2@gmail.com>
2024-01-31 19:15:40 -0300

    debounceTime

--- public/app/app.js
+++ public/app/app.js
@@ -1,7 +1,13 @@
 import { log } from "./utils/promise-help.js";
 import "./utils/array-help.js";
 import { notasService as service } from "./nota/service.js";
+import { debounceTime, takeUntil } from "./utils/operators.js";
 
+const action = debounceTime(
+  500,
+  takeUntil(3, () =>
+    service.sumItems("2143").then(console.log).catch(console.log)
+  )
+);
 
-document.querySelector("#myButton").onclick = () =>
-  service.sumItems("2143").then(console.log).catch(console.log);
+document.querySelector("#myButton").onclick = action;
--- public/app/utils/operators.js
+++ public/app/utils/operators.js
@@ -9,3 +9,13 @@ export const pipe =
   (...fns) =>
   (value) =>
     fns.reduce((previusValue, fn) => fn(previusValue), value);
+
+export const takeUntil = (times, fn) => () => times-- > 0 && fn();
+
+export const debounceTime = (milliseconds, fn) => {
+  let timer = 0;
+  return () => {
+    clearTimeout(timer);
+    timer = setTimeout(fn, milliseconds);
+  };
+};
