7172e034d65cc3a5a82fa03b372b23442e3902ab
Gleidson Queiroz <gldsnqueiroz2@gmail.com>
2024-01-30 21:11:09 -0300

    partialize

--- public/app/app.js
+++ public/app/app.js
@@ -1,15 +1,7 @@
-import { handleStatus, log } from "./utils/promise-help.js";
+import { log } from "./utils/promise-help.js";
 import "./utils/array-help.js";
+import { notasService as service } from "./nota/service.js";
 
-const sumItems = (code) => (notas) =>
-  notas
-    .$flatMap((nota) => nota.itens)
-    .filter((item) => item.codigo == code)
-    .reduce((total, item) => total + item.valor, 0);
 
 document.querySelector("#myButton").onclick = () =>
-  fetch("http://localhost:3000/notas")
-    .then(handleStatus)
-    .then(sumItems("213"))
-    .then(console.log)
-    .catch(console.log);
+  service.sumItems("2143").then(console.log).catch(console.log);
---
+++ public/app/nota/service.js
@@ -0,0 +1,22 @@
+import { handleStatus } from "../utils/promise-help.js";
+import { partialize } from "../utils/operators.js";
+
+const API = "http://localhost:3000/notas";
+
+const getItemsFromNotas = (notas) => notas.$flatMap((nota) => nota.itens);
+const filterItemsByCode = (code, items) =>
+  items.filter((item) => item.codigo == code);
+const sumItemsValue = (items) =>
+  items.reduce((total, item) => total + item.valor, 0);
+
+export const notasService = {
+  listAll() {
+    return fetch(API).then(handleStatus);
+  },
+
+  sumItems(code) {
+    const filterItems = partialize(filterItemsByCode, code);
+
+    return this.listAll().then(sumItems(code));
+  },
+};
