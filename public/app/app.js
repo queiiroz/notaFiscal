8bfbf3b34ad0c688439e719ac568a88451f224c9
Gleidson Queiroz <gldsnqueiroz2@gmail.com>
2024-02-22 20:37:51 -0300

    finish

--- public/app/app.js
+++ public/app/app.js
@@ -3,6 +3,7 @@ import { log, timeoutPromise, retry } from './utils/promise-helpers.js';
 import './utils/array-helpers.js';
 import { notasService as service } from './nota/service.js';
 import { takeUntil, debounceTime, partialize, pipe } from './utils/operators.js';
+import { EventEmitter } from './utils/event-emitter.js';
 
 const operations = pipe(
     partialize(takeUntil, 3),
@@ -11,7 +12,7 @@ const operations = pipe(
 
 const action = operations(() => 
     retry(3, 3000, () => timeoutPromise(200, service.sumItems('2143')))
-    .then(console.log)
+    .then(total => EventEmitter.emit('itensTotalizados', total))
     .catch(console.log)
 );
 
--- public/app/nota/service.js
+++ public/app/nota/service.js
@@ -1,11 +1,12 @@
 import { handleStatus } from '../utils/promise-helpers.js';
 import { partialize, pipe } from '../utils/operators.js';
+import { Maybe } from '../utils/maybe.js';
 
 const API = 'http://localhost:3000/notas';
 
-const getItemsFromNotas = notas => notas.$flatMap(nota => nota.itens);
-const filterItemsByCode = (code, items) => items.filter(item => item.codigo == code);
-const sumItemsValue = items => items.reduce((total, item) => total + item.valor, 0);
+const getItemsFromNotas = notasM => notasM.map(notas => notas.$flatMap(nota => nota.itens));
+const filterItemsByCode = (code, itemsM) => itemsM.map(items => items.filter(item => item.codigo == code));
+const sumItemsValue = itemsM => itemsM.map(items => items.reduce((total, item) => total + item.valor, 0));
 
 export const notasService = {
 
@@ -13,6 +14,7 @@ export const notasService = {
 
         return fetch(API)
             .then(handleStatus)
+            .then(notas => Maybe.of(notas))
             .catch(err => {
                 console.log(err);
                 return Promise.reject('Não foi possível obter as notas fiscais');
@@ -29,6 +31,7 @@ export const notasService = {
         );
 
         return this.listAll()
-            .then(sumItems);
+            .then(sumItems)
+            .then(result => result.getOrElse(0))
     }
 };
